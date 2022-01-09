0b61f8c53ca353e5ad3ebc81a02af8f195ae6ec5
SiddharthB70 <siddharth7002b@gmail.com>
2022-01-09 18:31:45 +0530

    Added Colors

--- script.js
+++ script.js
@@ -24,6 +24,75 @@ class Graph{
     addEdge(node1,node2,weight = 1){
         this.edges[node1.coordinate].push({node:node2.coordinate,weight: weight});        
     }
+
+    returnNode(node){
+        for(let reqNode of this.nodes){
+            if(node[0] == reqNode.x && node[1] == reqNode.y)
+                return reqNode;
+        }
+    }
+
+    Dijkstras(start, finish) {
+        const distanceFromStart = {};
+        const nextCheck = new PriorityQueue();
+        const visited = {};
+
+        let current;
+        let result = [];
+        for (let vert of this.nodes) {
+            if (vert.x==start[0] && vert.y==start[1]) {
+                distanceFromStart[vert.coordinate] = 0;
+                nextCheck.enqueue(vert.coordinate, 0);
+            } 
+            else {
+                distanceFromStart[vert.coordinate] = Infinity;
+            }
+            visited[vert.coordinate] = null;
+        }
+
+       
+        
+        while (nextCheck.values.length) {
+            current = nextCheck.dequeue().val;
+            if(current[0]==finish[0]&&current[1]==finish[1])
+            {
+                while(visited[current]){
+                    result.push(current);
+                    current = visited[current];
+                }
+                break;
+            }
+            else{
+                for (let neighbor of this.edges[current]) {
+                    
+                    let distanceToNeighbor = distanceFromStart[current] + neighbor.weight;
+                   
+                    if(distanceToNeighbor < distanceFromStart[neighbor.node]){
+                        distanceFromStart[neighbor.node] = distanceToNeighbor;
+                        visited[neighbor.node] = current;
+                        nextCheck.enqueue(neighbor.node, distanceFromStart);
+                    }
+                }
+            }
+        }
+        colorPath(result.reverse());  
+    }
+}
+
+class PriorityQueue {
+    constructor(){
+        this.values = [];
+    }
+    enqueue(val, priority) {
+        this.values.push({val, priority});
+        this.sort();
+    };
+    dequeue() {
+        return this.values.shift();
+    };
+    sort() {
+        this.values.sort((a, b) => a.priority - b.priority);
+    };
 }
 
 function createGrid(){
@@ -71,6 +140,26 @@ function addEdges(){
     }
 }
 
+function FindStartNode(node){
+    let reqNode = graph.returnNode(node);
+    reqNode.cell.style.backgroundColor = "crimson";
+}
+
+function FindEndNode(node){
+    let reqNode = graph.returnNode(node);
+    reqNode.cell.style.backgroundColor = "blue";
+}
+
+function colorPath(result){
+    for (let node of result){
+        let reqNode = graph.returnNode(node);
+        reqNode.cell.style.backgroundColor = "cyan";
+    }
+}
+
 const graph = new Graph();
 createGrid();
+graph.Dijkstras([14,12],[1,3]);
+FindStartNode([14,12]);
+FindEndNode([1,3]);
 
